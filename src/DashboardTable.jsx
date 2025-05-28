c0f44ad12ff58e82fc91a17a884c59637adb75dd
irene-love-bluesea <eaindraychu32@gmail.com>
2025-05-28 12:44:25 +0700

    Add Download Function

--- src/DashboardTable.jsx
+++ src/DashboardTable.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import * as XLSX from 'xlsx';
+import * as htmlToImage from 'html-to-image';
+import download from 'downloadjs';
 import './DashboardTable.css';
 
 const DashboardTable = () => {
@@ -11,20 +14,44 @@ const DashboardTable = () => {
     navigate('/', { replace: true });
   };
 
-   if (!Array.isArray(data) || data.length === 0) {
+  const handleDownloadExcel = () => {
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Employees");
+    XLSX.writeFile(workbook, "Employee_dashboard.xlsx");
+  };
+
+  const handleDownloadPNG = () => {
+    const tableNode = document.getElementById('dashboard-table');
+
+    htmlToImage.toPng(tableNode).then((dataUrl) => {
+      download(dataUrl, 'Employee_dashboard.png');
+    }).catch((err) => {
+      console.error('Error generating PNG:', err);
+    });
+  };
+
+  if (!Array.isArray(data) || data.length === 0) {
     return (
       <div className="table-wrapper">
         <h3>No employee data available to display</h3>
-      <p>Please Double Check your Data or Files!!</p>
-      <button className="backbtn" onClick={handleGoBack}>Go Back to Upload</button>
+        <p>Please Double Check your Data or Files!!</p>
+        <button className="backbtn" onClick={handleGoBack}>Go Back to Upload</button>
       </div>
     );
   }
-  
+
   return (
     <div className="table-wrapper">
-      <h2 className="dashboard-heading">New Employee Dashboard</h2>
-      <table className="dashboard-table">
+      <div className="table-header">
+        <div><h2 className="dashboard-heading">New Employee Dashboard</h2></div>       
+        <div className="download-buttons">
+          <div><button onClick={handleDownloadExcel}>Download Excel</button></div>
+          <div><button onClick={handleDownloadPNG}>Download PNG</button></div>
+        </div>
+      </div>
+
+      <table className="dashboard-table" id="dashboard-table">
         <thead>
           <tr>
             <th>No.</th>
@@ -46,7 +73,8 @@ const DashboardTable = () => {
           ))}
         </tbody>
       </table>
-            <button className="backbtn" onClick={handleGoBack}>Go Back to Upload</button>
+
+      <button className="backbtn" onClick={handleGoBack}>Go Back to Upload</button>
     </div>
   );
 };
